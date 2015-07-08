54d3084e4e537323cf97056f5d03c203deebb633
airrick213 <erickim213@gmail.com>
2015-07-08 15:53:56 -0400

    Initial Commit

---
+++ GasShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GasShare
+//
+//  Created by Eric Kim on 7/8/15.
+//  Copyright (c) 2015 Eric Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
