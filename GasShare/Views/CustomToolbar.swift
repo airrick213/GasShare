4675d69f5821efe0554d59f2e078778c69413fcf
airrick213 <erickim213@gmail.com>
2015-08-10 13:42:54 -0400

    moar car picker ui fixing

--- GasShare/Views/CustomToolbar.swift
+++ GasShare/Views/CustomToolbar.swift
@@ -10,18 +10,4 @@ import UIKit
 
 class CustomToolbar: UIView {
 
-    override func drawRect(rect: CGRect) {
-        super.drawRect(rect)
-        
-        self.backgroundColor = UIColor.blueColor()
-        
-        let gradient = CAGradientLayer()
-        gradient.frame = self.bounds
-        
-        gradient.colors = [UIColor.whiteColor(), UIColor(hue: 359.0/360.0, saturation: 0, brightness: 0.85, alpha: 1.0)]
-        
-        let sublayerCount: UInt32 = UInt32(self.layer.sublayers!.count + 1)
-        //self.layer.insertSublayer(gradient, atIndex: sublayerCount)
-    }
-
 }
