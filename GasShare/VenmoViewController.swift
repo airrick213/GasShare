56ac0f03f359ecf3914ed2e750e3b9b5d442501c
airrick213 <erickim213@gmail.com>
2015-08-10 16:10:47 -0400

    unnecessary comment

--- GasShare/VenmoViewController.swift
+++ GasShare/VenmoViewController.swift
@@ -88,16 +88,6 @@ class VenmoViewController: UIViewController {
         MBProgressHUD.hideAllHUDsForView(view, animated: true)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension VenmoViewController: UITextFieldDelegate {
